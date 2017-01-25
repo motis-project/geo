139b0b2bbca71081578cb7aa40b887c94bcaf06e
Sebastian Fahnenschreiber <fahnenschreiber@algo.informatik.tu-darmstadt.de>
2017-01-25 19:51:27 +0100

    polyline: predefined simplify factors

--- include/geo/polyline.h
+++ include/geo/polyline.h
@@ -10,7 +10,28 @@ using polyline = std::vector<latlng>;
 
 double length(polyline const&);
 
-polyline simplify(polyline const&);
+polyline simplify(polyline const&, double const);
+
+template <int TileSize, int MaxZoomLevel = 20>
+polyline simplify(polyline const& polyline, uint32_t const z) {
+  constexpr auto kMinPixel = 5;
+
+  struct look_up_table {
+    constexpr look_up_table() : values() {
+      for (auto i = 0; i <= MaxZoomLevel; ++i) {
+        double shift = (1u << i) * TileSize;
+        double b = shift / 2.0;
+        double pixel_to_deg = 180. / b;
+        double min_deg = kMinPixel * pixel_to_deg;
+        values[i] = min_deg * min_deg;
+      }
+    }
+    double values[MaxZoomLevel + 1];
+  };
+  constexpr auto lut = look_up_table{};
+
+  return simplify(polyline, lut.values[z]);
+}
 
 polyline extract(polyline const&, size_t const from, size_t const to);
 
