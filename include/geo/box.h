d128c30cd4ce5e93491f6119e36648b697d18187
Sebastian Fahnenschreiber <sebastian.fahnenschreiber@gmail.com>
2017-12-06 09:32:06 +0100

    box: extend by distance (meters)

--- include/geo/box.h
+++ include/geo/box.h
@@ -1,7 +1,9 @@
 #pragma once
 
+#include <cmath>
 #include <limits>
 
+#include "geo/constants.h"
 #include "geo/latlng.h"
 #include "geo/polyline.h"
 
@@ -35,6 +37,28 @@ struct box {
 
     min_.lat_ = std::min(min_.lat_, pos.lat_);
     min_.lng_ = std::min(min_.lng_, pos.lng_);
+  }
+
+  void extend(double dist_in_m) {
+    // The distance of latitude degrees in km is always the same (~111000.0f)
+    double const d_lat = dist_in_m / 111000.0f;
+
+    min_.lat_ -= d_lat;
+    max_.lat_ += d_lat;
+
+    // The distance of longitude degrees depends on the latitude.
+    double const min_lat_rad = min_.lat_ * (kPI / 180.0f);
+    double const min_m_per_deg = 111200.0f * std::cos(min_lat_rad);
+    double const min_d_lng = std::abs(dist_in_m / min_m_per_deg);
+
+    min_.lng_ -= min_d_lng;
+
+    // The distance of longitude degrees depends on the latitude.
+    double const max_lat_rad = max_.lat_ * (kPI / 180.0f);
+    double const max_m_per_deg = 111200.0f * std::cos(max_lat_rad);
+    double const max_d_lng = std::abs(dist_in_m / max_m_per_deg);
+
+    max_.lng_ -= max_d_lng;
   }
 
   bool contains(latlng const& pos) const {
