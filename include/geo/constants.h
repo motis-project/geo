b35c3f20c11914ecf19d7be0c1825fa56387e7c9
Traines <git@traines.eu>
2025-06-02 10:21:23 +0000

    approx squared distance

--- include/geo/constants.h
+++ include/geo/constants.h
@@ -5,5 +5,7 @@ namespace geo {
 constexpr auto kPI = 3.14159265358979323846;
 constexpr auto kEarthRadiusMeters = 6371000.0;
 constexpr auto kEpsilon = 0.000000001;
+constexpr auto kApproxDistanceLatDegrees =
+    geo::kEarthRadiusMeters * geo::kPI / 180;
 
 }  // namespace geo
