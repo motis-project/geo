bd0f360c6f899ac6440aaff7b1d3257fbf373d3b
mority <mority@posteo.net>
2026-08-03 21:00:49 +0200

    adds tests

---
+++ test/etrs89_laea_test.cc
@@ -0,0 +1,31 @@
+#include "doctest/doctest.h"
+
+#include "geo/etrs89_laea.h"
+
+using namespace geo;
+
+TEST_CASE("from_etrs89_laea") {
+  auto const darmstadt = from_etrs89_laea(4224037, 2974258);
+  CHECK(darmstadt.lng_ == doctest::Approx(8.651222));
+  CHECK(darmstadt.lat_ == doctest::Approx(49.872833));
+
+  auto const lisboa = from_etrs89_laea(2665343, 1945115);
+  CHECK(lisboa.lng_ == doctest::Approx(-9.13702));
+  CHECK(lisboa.lat_ == doctest::Approx(38.707482));
+
+  auto const sulina = from_etrs89_laea(5848671, 2652429);
+  CHECK(sulina.lng_ == doctest::Approx(29.654932));
+  CHECK(sulina.lat_ == doctest::Approx(45.155896));
+
+  auto const hornstrandir = from_etrs89_laea(2920104, 5141803);
+  CHECK(hornstrandir.lng_ == doctest::Approx(-22.443008));
+  CHECK(hornstrandir.lat_ == doctest::Approx(66.362433));
+
+  auto const utsjoki = from_etrs89_laea(4974179, 5278480);
+  CHECK(utsjoki.lng_ == doctest::Approx(27.025337));
+  CHECK(utsjoki.lat_ == doctest::Approx(69.907579));
+
+  auto const valletta = from_etrs89_laea(4732145, 1438675);
+  CHECK(valletta.lng_ == doctest::Approx(14.513626));
+  CHECK(valletta.lat_ == doctest::Approx(35.899480));
+}
